6aa05d166b185f6d5c613c5babcc0fd08f921ebe
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-09-08 10:47:56 +0200

    new routes for clients

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ClientsComponent } from './clients/clients.component';
+import { NewComponent } from './clients/new/new.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'clients', component: ClientsComponent },
+  { path: 'clients/new', component: NewComponent }
+];
