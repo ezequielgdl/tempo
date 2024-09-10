63cc4408d5a9b0481fac73670ba2863e7555e518
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-09-10 19:47:05 +0200

    auth guard update rxjs

--- src/app/auth-guard.guard.ts
+++ src/app/auth-guard.guard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './services/auth-service.service';
 import { User } from '@supabase/supabase-js';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +11,17 @@ import { User } from '@supabase/supabase-js';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  async canActivate(): Promise<boolean> {
-    const user: User | null = await this.authService.isLoggedIn();
-    if (user) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
+  canActivate(): Observable<boolean> {
+    return this.authService.getCurrentUser().pipe(
+      take(1),
+      map((user: User | null) => {
+        if (user) {
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      })
+    );
   }
 }
